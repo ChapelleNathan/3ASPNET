ef543b6ef3d823d25ba7ad9f5b636bbe1194e03a
Nathan Chapelle <ncdev@outlook.fr>
2024-02-05 15:10:50 +0100

    i did shit

---
+++ 3ASP/Data/DataContext.cs
@@ -0,0 +1,10 @@
+namespace _3ASP.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    // public DbSet<User> Users => Set<User>();
+}
