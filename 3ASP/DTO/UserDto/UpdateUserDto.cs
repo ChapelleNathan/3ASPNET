bb1de312a3a1d4d7bc065ff68981adbcc2372ab8
Halphane <ncdev@outlook.fr>
2024-02-10 15:55:30 +0100

    modified user dtos, modification of password should be different

--- 3ASP/DTO/UserDto/UpdateUserDto.cs
+++ 3ASP/DTO/UserDto/UpdateUserDto.cs
@@ -7,5 +7,4 @@ public class UpdateUserDto
     public int Id { get;   set; }
     public string Pseudo { get;  set; }
     public string Email { get;  set; }
-    public string Password { get;  set; }
 }
